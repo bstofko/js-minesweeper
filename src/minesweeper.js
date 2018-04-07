8ae5305dad7d2733026e2b85f1a69838427bba47
bstofko <bob.stofko@gmail.com>
2018-04-06 19:48:01 -0500

    First commit

---
+++ src/minesweeper.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+/*(const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard); */
+
+/* board[0][0] = '0,0';
+board[0][1] = '0,1';
+board[0][2] = '0,2';
+board[1][0] = '1,0';
+board[1][1] = '1,1';
+board[1][2] = '1,2';
+board[2][0] = '2,0';
+board[2][1] = '2,1';
+board[2][2] = '2,2'; */
